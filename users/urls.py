f3c874efb0fa5011d05aa9b9feeff4adc375f8cf
mateusz <mateusz.lobocki@stxnext.pl>
2014-08-12 20:52:56 +0200

    adding register form and logout function

--- users/urls.py
+++ users/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns(
     url(r'^login/', views.authenticate_user, name='login'),
     url(r'^login_show/', views.login_show, name='login_show'),
     url(r'^$', views.login_show),
-    # url(r'^logout/', views.logout),
-    # url(r'^register/', views.register),
+    url(r'^logout/', views.logout_view, name='logout_view'),
+    url(r'^register/', views.register, name='register'),
     url(r'^profile/', views.profile, name='profile'),
     # url(r'^profile/edit/', views.edit),
 )
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from forms import register_form
 
 
 def authenticate_user(request):
-
+    """Authentication method"""
     username = request.POST['username']
     password = request.POST['password']
     user = authenticate(username=username, password=password)
@@ -22,12 +25,33 @@ def authenticate_user(request):
 
 
 def login_show(request):
-
+    """Login form view"""
     return render(request, 'login_show.html')
 
 
 def profile(request):
+    """Main page view"""
     if request.user.is_authenticated():
         return render(request, 'profile.html')
     else:
         return redirect('login_show')
+
+def register(request):
+    """User registration view"""
+    if request.method == 'POST':
+        form = register_form(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+                email=form.cleaned_data['email'],
+            )
+            user.save()
+            return HttpResponse('dupka')
+    elif request.method == 'GET':
+        return render(request, 'register.html')        
+
+def logout_view(request):
+
+    logout(request)
+    return HttpResponse('Logout')
