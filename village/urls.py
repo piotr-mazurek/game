41408e51cfacb27fd50e31f20b251cae3292743d
prmazurek <przemek.m.mazurek@gmail.com>
2014-08-12 20:49:16 +0200

    working on village module

--- village/urls.py
+++ village/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 from village import views
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', views.overview, name='overview'),
     url(r'^upgrade/(?P<building_id>\d+)/$', views.upgrade, name='upgrade'),
     url(
-    	r'^downgrade/(?P<building_id>\d+)/$', views.downgrade, name='downgrade'
-    	),
+        r'^downgrade/(?P<building_id>\d+)/$', views.downgrade, name='downgrade'
+        ),
 )
